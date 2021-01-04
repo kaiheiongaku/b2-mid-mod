b26ee92c2c080ba61d183b02d0ae687bc92936ea
Robert Heath <robert.b.heathii@gmail.com>
2021-01-04 10:50:36 -0600

    Create models

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe, type: :model do
+RSpec.describe Movie, type: :model do
   describe 'relationships' do
     it {should belong_to :studio}
     it {should have_many :movie_actors}
