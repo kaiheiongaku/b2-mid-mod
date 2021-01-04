e7a93e8a189191441fcfc82a79cba28889b7f5ad
Robert Heath <robert.b.heathii@gmail.com>
2021-01-04 12:52:48 -0600

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Studio.destroy_all
+Actor.destroy_all
+Movie.destroy_all
+MovieActor.destroy_all
+
+fox = Studio.create!(name: "Fox", location: "California")
+disney = Studio.create!(name: "Disney", location: "Florida")
+
+star_wars = Movie.create!(title: "Star Wars", creation_year: 2019, studio: disney, genre: "scifi")
+frozen = Movie.create!(title: "Frozen", creation_year: 2010, studio: disney, genre: "fantasy")
+
+mhamil = Actor.create!(name: "Mark Hamil", age: 60)
+cfischer = Actor.create!(name: "Carrie Fischer", age: 65)
+emenzel = Actor.create!(name: "Edina Menzel", age: 45)
+kbell = Actor.create!(name: "Kristen Bell", age: 44)
+
+MovieActor.create!(movie: frozen, actor: emenzel)
+MovieActor.create!(movie: frozen, actor: kbell)
+MovieActor.create!(movie: star_wars, actor: mhamil)
+MovieActor.create!(movie: star_wars, actor: cfischer)
